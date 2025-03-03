aa80361b5e32655c7557f51ec04887ee0dcbbc5a
Olga <122048357+ParfinovichOlga@users.noreply.github.com>
2025-03-03 10:44:33 +0300

    Added filtering by id, table_number, status

--- api_orders/views.py
+++ api_orders/views.py
@@ -5,6 +5,14 @@ Views for order APIs.
 from rest_framework import viewsets
 from orders.models import *
 from api_orders import serializers
+from drf_spectacular.utils import(
+    extend_schema_view,
+    extend_schema,
+    OpenApiParameter,
+    OpenApiTypes,
+)
+
+ORDER_STATUS = ['processed', 'ready', 'paid']
 
 
 class DishViewSet(viewsets.ModelViewSet):
@@ -28,11 +36,42 @@ class MenuViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.MenuSerializer
     queryset = Menu.objects.all()
 
-
+@extend_schema_view(
+    list = extend_schema(
+        parameters=[
+            OpenApiParameter(
+                'table_number',
+                OpenApiTypes.STR
+            ),
+            OpenApiParameter(
+                'id',
+                OpenApiTypes.STR
+            ),
+            OpenApiParameter(
+                'status',
+                OpenApiTypes.STR, enum = ORDER_STATUS
+            ),
+        ]
+    )
+)
 class OrderViewSet(viewsets.ModelViewSet):
     """Manage order."""
     serializer_class = serializers.OrderDetailSerializer
-    queryset = Order.objects.all().order_by('-id')
+    queryset = Order.objects.all()
+
+    def get_queryset(self):
+        table_number = self.request.query_params.get('table_number')
+        id = self.request.query_params.get('id')
+        status = self.request.query_params.get('status')
+        queryset = self.queryset
+        if table_number:
+            queryset = queryset.filter(table_number=table_number)
+        if id:
+            queryset = queryset.filter(id=id)
+        if status:
+            queryset = queryset.filter(status=status)
+        return queryset.order_by('-id')
+
 
     def get_serializer_class(self):
         """Return the serializer class for request."""
